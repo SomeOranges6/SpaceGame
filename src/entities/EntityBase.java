b92dbd9b29c8f71e407ca748ec15341439c7003d
SomeOranges6 <363374786@gotvdsb.ca>
2024-06-04 09:11:39 -0400

    Update EntityBase.java

--- src/entities/EntityBase.java
+++ src/entities/EntityBase.java
@@ -1,5 +1,37 @@
 package entities;
 
-public class EntityBase {
+import java.awt.Rectangle;
+import java.util.Random;
 
+@SuppressWarnings("serial")
+public abstract class EntityBase extends Rectangle {
+	
+	public int x,y;
+	
+	static Random rand = new Random();
+	
+    public int vX, vY;
+    
+    public int health = 1;
+    
+    public int width,height;
+    
+    public EntityBase(int x, int y, int width, int height) {
+		super(x,y,width,height);
+	}
+    
+    public void onDead() {
+    	
+    }
+    
+    public void update() {
+    	
+    }
+    
+    
+    
+    
+    
+    
+    
 }
