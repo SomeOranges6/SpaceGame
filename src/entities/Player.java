fb7d8caf7368dd5f4339a268eaa8d01dd1048f95
SomeOranges6 <363374786@gotvdsb.ca>
2024-06-05 09:09:56 -0400

    Basic Game Structure

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -1,5 +1,11 @@
 package entities;
 
-public class Player {
+@SuppressWarnings("serial")
+public class Player extends EntityBase {
+
+	public Player(int x, int y, int width, int height) {
+		super(x, y, width, height);
+	}
+	
 
 }
