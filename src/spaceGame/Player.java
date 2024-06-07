1bd17168165bcf32507da77f57ac83f032841e13
rajdeepchowdhury339 <362840118@gotvdsb.ca>
2024-06-06 21:42:14 -0400

    Update Player.java

--- src/spaceGame/Player.java
+++ src/spaceGame/Player.java
@@ -3,8 +3,8 @@ package spaceGame;
 @SuppressWarnings("serial")
 class Player extends GameEntities {
 	int firerate = 500; //in ms
-	public int fasterSpd, jetFuel;
-	public int hp, iFrames; //iFrames are in ms
+	public int fasterSpd, jetFuel, fuelDrain;
+	public int hp, iFrames, active_iFrames; //iFrames are in ms
 	public int rotation=0; //in degrees, rotation is used to normalize movement. it constantly changes.
 	
 	Player() {
@@ -13,10 +13,12 @@ class Player extends GameEntities {
 		this.spd=3;
 		this.fasterSpd=8;
 		this.jetFuel=500;
+		this.fuelDrain = 5;
 		this.firerate = 500;
 		this.hp = 3;
-		this.iFrames=1000; //The intention is firerate of enemies divide and player iframes multiply by 1.5 on medium, 2x on easy.
-		this.size=14;
+		this.iFrames=750; //The intention is firerate of enemies divide and player iframes multiply by 1.5 on medium, 2x on easy.
+		this.active_iFrames=0;
+		this.size=20;
 		this.rotation=0;
 	}
 	
@@ -26,9 +28,11 @@ class Player extends GameEntities {
 		this.spd=2;
 		this.fasterSpd=5;
 		this.jetFuel=500;
+		this.fuelDrain = 1;
 		this.firerate = 500;
 		this.hp = 3;
-		this.iFrames=1000;
+		this.iFrames=750;
+		this.active_iFrames=0;
 		this.size=20;
 		this.rotation=0;
 	}
