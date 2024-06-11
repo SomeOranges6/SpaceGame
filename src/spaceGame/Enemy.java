c45fb07705f08ef7f1f04cd313861fead08d535b
rajdeepchowdhury339 <362840118@gotvdsb.ca>
2024-06-11 18:52:07 -0400

    Update Enemy.java

--- src/spaceGame/Enemy.java
+++ src/spaceGame/Enemy.java
@@ -2,8 +2,15 @@ package spaceGame;
 
 @SuppressWarnings("serial")
 abstract class Enemy extends GameEntities {
-	public int hp, iFrames;
+	public int hp, iFrames; //iFrames are not used
 	public int firerate, fireBuffer, untilFire;
+	public double rotation;
+	int displacement = 0, rotationSpd, pointX, pointY; //for rotater
+	double findRotation(int x1, int y1, int x2, int y2) {
+		double rotation = 0;
+		rotation = Math.toDegrees(Math.atan2(y2-y1, x2-x1));
+		return rotation; 
+	}
 }
 
 @SuppressWarnings("serial")
@@ -18,7 +25,8 @@ class Liner extends Enemy {
 		this.untilFire=0;
 		this.x=WINB/2;
 		this.y=0;
-		this.spd = 1;
+		this.spd = 2;
+		this.rotation = randNum.nextInt(-135,-45+1)*-1;
 	}
 	
 	Liner(int x, int y) {
@@ -30,7 +38,8 @@ class Liner extends Enemy {
 		this.untilFire=0;
 		this.x=x;
 		this.y=y;
-		this.spd = 1;
+		this.spd = 2;
+		this.rotation = randNum.nextInt(-135,-45+1)*-1;
 	}
 
 }
@@ -41,34 +50,88 @@ class Tanker extends Enemy {
 	Tanker() {
 		this.hp=4;
 		this.iFrames=75;
-		this.size=20;
+		this.size=24;
 		this.firerate=1000;
 		this.fireBuffer=0;
 		this.untilFire=0;
 		this.x=WINB/2;
 		this.y=0;
-		this.spd = 2;
+		this.spd = 3;
+		this.rotation = 0;
 	}
 	
 	Tanker(int x, int y) {
 		this.hp=4;
 		this.iFrames=75;
-		this.size=20;
+		this.size=24;
 		this.firerate=1000;
 		this.fireBuffer=0;
 		this.untilFire=0;
 		this.x=x;
 		this.y=y;
-		this.spd = 2;
+		this.spd = 3;
+		this.rotation = 0;
 	}
 	
 }
-/*
-//Rotates around a point that may move
-class Rotater extends Enemy {
 
+//Rotates around a point that may move
+@SuppressWarnings("serial")
+class Rotater extends Enemy { 
+	//x and y are the points they rotate, displacement is how far from the point
+	Rotater() {
+		this.hp=1;
+		this.iFrames=75;
+		this.size=16;
+		this.firerate=1000;
+		this.fireBuffer=0;
+		this.untilFire=0;
+		this.x=0;
+		this.y=0;
+		this.pointX = x;
+		this.pointY = y;
+		this.spd = 1;
+		this.rotationSpd = 2;
+		this.displacement = 0;
+		this.rotation = 0;
+	}
+	
+	Rotater(int x, int y, int displacement) {
+		this.hp=1;
+		this.iFrames=75;
+		this.size=16;
+		this.firerate=1000;
+		this.fireBuffer=0;
+		this.untilFire=0;
+		this.x=0;
+		this.y=0;
+		this.rotationSpd = 2;
+		this.pointX = x;
+		this.pointY = y;
+		this.displacement = displacement;
+		this.spd = 1;
+		this.rotation = 0;
+	}
+	
+	Rotater(int x, int y, int displacement, int rotation) {
+		this.hp=1;
+		this.iFrames=75;
+		this.size=16;
+		this.firerate=1000;
+		this.fireBuffer=0;
+		this.untilFire=0;
+		this.x=0;
+		this.y=0;
+		this.pointX = x;
+		this.pointY = y;
+		this.displacement = displacement;
+		this.spd = 1;
+		this.rotationSpd = 5;
+		this.rotation = rotation;
+	}
 }
 
+/*
 //shoots really fast, essentially limiting the arena
 class Beamer extends Enemy {
 
